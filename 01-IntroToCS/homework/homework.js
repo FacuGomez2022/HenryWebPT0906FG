459458e0d2c23c19fd32b0eb1c63921433804076
Facundo Gomez <fgfacundogomez@hotmail.com>
2022-08-03 17:01:30 -0300

    Homework 01 done

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,27 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+    let sum = 0;
 
+    for (let i = 0; i < num.length; i++) {
+       sum = sum + num[i] *  2 ** (num.length - 1 - i);
+    }
+    return sum;
 }
+
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  //  7            4
+  // 7/2=3(1)   4/2=2(0)
+  // 3/2=1(1)   2/2=1(0)
+  // 1/2=0(1)   1/2=0(1) 
+  let binary = "";
+  while (num !== 0) { 
+    binary = num%2 + binary;
+     num = Math.floor(num / 2)
+  }
+  return binary
 }
 
 
