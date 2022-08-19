672a45fc73ae5c0610652f26476be502e2173b9a
Facundo Gomez <fgfacundogomez@hotmail.com>
2022-08-19 19:19:28 -0300

    homework05

--- 05-EstructuraDeDatos-II/homework/homework.js
+++ 05-EstructuraDeDatos-II/homework/homework.js
@@ -96,20 +96,89 @@ LinkedList.prototype.search = function(data){
 
 /*
 Implementar la clase HashTable.
-
-Nuetra tabla hash, internamente, consta de un arreglo de buckets (slots, contenedores, o casilleros; es decir, posiciones posibles para almacenar la información), donde guardaremos datos en formato clave-valor (por ejemplo, {instructora: 'Ani'}).
-Para este ejercicio, la tabla debe tener 35 buckets (numBuckets = 35). (Luego de haber pasado todos los tests, a modo de ejercicio adicional, pueden modificar un poco la clase para que reciba la cantidad de buckets por parámetro al momento de ser instanciada.)
-
+Nuetra tabla hash, internamente, consta de un arreglo de buckets (slots, contenedores, o casilleros; es decir,
+posiciones posibles para almacenar la información), donde guardaremos datos en formato clave-valor (por ejemplo, {instructora: 'Ani'}).
+Para este ejercicio, la tabla debe tener 35 buckets (numBuckets = 35). (Luego de haber pasado todos los tests, a modo de ejercicio adicional,
+pueden modificar un poco la clase para que reciba la cantidad de buckets por parámetro al momento de ser instanciada.)
 La clase debe tener los siguientes métodos:
-  - hash: función hasheadora que determina en qué bucket se almacenará un dato. Recibe un input alfabético, suma el código numérico de cada caracter del input (investigar el método charCodeAt de los strings) y calcula el módulo de ese número total por la cantidad de buckets; de esta manera determina la posición de la tabla en la que se almacenará el dato.
-  - set: recibe el conjunto clave valor (como dos parámetros distintos), hashea la clave invocando al método hash, y almacena todo el conjunto en el bucket correcto.
+  - hash: función hasheadora que determina en qué bucket se almacenará un dato. 
+  Recibe un input alfabético, suma el código numérico de cada caracter del input (investigar el método charCodeAt de los strings) y 
+  calcula el módulo de ese número total por la cantidad de buckets;
+   de esta manera determina la posición de la tabla en la que se almacenará el dato.
+  - set: recibe el conjunto clave valor (como dos parámetros distintos), hashea la clave invocando al método hash,
+   y almacena todo el conjunto en el bucket correcto.
   - get: recibe una clave por parámetro, y busca el valor que le corresponde en el bucket correcto de la tabla.
   - hasKey: recibe una clave por parámetro y consulta si ya hay algo almacenado en la tabla con esa clave (retorna un booleano).
 
 Ejemplo: supongamos que quiero guardar {instructora: 'Ani'} en la tabla. Primero puedo chequear, con hasKey, si ya hay algo en la tabla con el nombre 'instructora'; luego, invocando set('instructora', 'Ani'), se almacenará el par clave-valor en un bucket específico (determinado al hashear la clave)
 */
+function HashTable() {
+  //arreglo de objetos
+  this.numBuckets = 35;
+  this.buckets = [];
+}
 
-function HashTable() {}
+HashTable.prototype.hash = function (key){
+  let sum = 0;
+  for (let i = 0; i < key.length; i++){
+    sum += key.charCodeAt(i);
+  }
+  return sum%this.numBuckets
+};
+HashTable.prototype.set = function (key, value){
+  //revisa que key si sea un string
+  if (typeof key !== 'string') throw new TypeError("Key must be strings");
+  let posArr = this.hash(key);
+  //si la posición en el arreglo está vacía, crea un objeto para guardar los valores
+  if (this.buckets[posArr] === undefined) {
+    this.buckets[posArr] = {};
+  }
+  this.buckets[posArr][key] = value;
+}
+HashTable.prototype.get = function (key){
+  let posArr = this.hash(key);
+  return this.buckets[posArr][key];
+}
+HashTable.prototype.hasKey = function(key){
+  let posArr = this.hash(key);
+  return this.buckets[posArr].hasOwnProperty(key);
+};
+/*
+function HashTable() {
+this.numBuckets = 35;
+this.array = [];
+}
+
+HashTable.prototype.hash  = function (key, value) { 
+let sum = 0;
+for (let i = 0; i < key.length; i++){
+  sum+= key.charCodeAt(i)
+  }
+  return sum%this.numBuckets
+}
+
+HashTable.prototype.set  = function (key, value) { 
+if (typeof key != 'string') throw new TypeError('Keys must be strings');
+let backet = this.hash(key);
+if (!this.array[backet]) {
+  this.array[backet] = {}
+}
+this.array[backet][key] = value;
+}
+
+
+
+HashTable.prototype.get  = function (key) {
+  let backet = this.hash(key);
+  return this.array[backet][key];
+}
+
+HashTable.prototype.hasKey  = function (key) {
+let backet = this.hash(key);
+return this.array[backet].hasOwnProperty(key) 
+  
+}
+*/
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
