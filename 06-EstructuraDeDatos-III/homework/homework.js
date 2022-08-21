e9f359e4626a5fb2a9d026fbee28d1237f6434bb
Facundo Gomez <fgfacundogomez@hotmail.com>
2022-08-21 20:43:07 -0300

    ready

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -10,58 +10,113 @@
 
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
-var abs = Math.abs;
-var max = Math.max;
-var min = Math.min;
-
 function BinarySearchTree(value) {
   this.value = value;
   this.left = null; //BinaryTree
   this.right = null; //BinaryTree
-}
-BinarySearchTree.prototype.size = function () {
-//HIEGHT
-};
-BinarySearchTree.prototype.contains = function (value) {};
-BinarySearchTree.prototype.depthFirstForEach = function (value) {};
-BinarySearchTree.prototype.breadthFirstForEach = function (value) {};
-BinarySearchTree.prototype.insert = function (value) {
- if (value > this.value) {
-  if (this.right !== null) {
-    this.right.insert(value);
-  } else {
-    this.right = new BinarySearchTree(value);
-  }
- } 
- if (value <= this.value) {
-  if (this.left !== null) {
-      this.left.insert(value);
-  } else {
-      this.left = new BinarySearchTree(value);
-  }
-  }
-};
-BinarySearchTree.prototype.contains = function (value) {
-/*
-if (this.value === value){
-  return true
-}
-if (value < this.value && this.left){
-return this.left.contains(value)
-}
-if (value > this.value && this.right){
-return this.right.contains(value)
-}
-return false
-*/
-  return (
-    (value === this.value ? true : false) || 
-    (this.left ? this.left.contains(value) : false) || 
-    (this.right ? this.right.contains(value) : false)
-  );
-  //* true/false  true/false  true/false
 };
 
+BinarySearchTree.prototype.size = function (suma = 1) { 
+  if (this.value === null){
+    return 0;
+  }; 
+  if (this.left === null && this.right === null){
+    return 1;
+  };
+  if (this.left === null){
+    return 1 + this.right.size();
+  };
+  if (this.right === null){
+    return 1 + this.left.size();
+  }; 
+  return 1 + this.left.size() + this.right.size();
+};
+
+BinarySearchTree.prototype.insert = function (value) {
+  if (value > this.value) {
+   if (this.right !== null) {
+     this.right.insert(value);
+   } else {
+     this.right = new BinarySearchTree(value);
+   }
+  } 
+  if (value <= this.value) {
+   if (this.left !== null) {
+       this.left.insert(value);
+   } else {
+       this.left = new BinarySearchTree(value);
+   }
+   }
+ };
+BinarySearchTree.prototype.contains = function (value) {
+  /*
+  if (this.value === value){
+    return true
+  }
+  if (value < this.value && this.left){
+  return this.left.contains(value)
+  }
+  if (value > this.value && this.right){
+  return this.right.contains(value)
+  }
+  return false
+  */
+ return (
+   (value === this.value ? true : false) || 
+   (this.left ? this.left.contains(value) : false) || 
+   (this.right ? this.right.contains(value) : false)
+ );
+// //* true/false  true/false  true/false
+};
+BinarySearchTree.prototype.breadthFirstForEach = function (
+  cb,
+  arrayFlotante = []
+) {
+  cb(this.value);
+  if (this.left) {
+    arrayFlotante.push(this.left);
+  }
+  if (this.right){
+    arrayFlotante.push(this.right);
+  }
+  if (arrayFlotante.length > 0){
+    arrayFlotante.shift().breadthFirstForEach(cb, arrayFlotante);
+  };
+}; 
+BinarySearchTree.prototype.depthFirstForEach = function (
+  cb,
+  order = "in-order"
+) {
+  switch(order){
+    case "in-order":
+      if (this.left){
+        this.left.depthFirstForEach(cb, order);
+      };
+      cb (this.value);
+      if (this.right) {
+        this.right.depthFirstForEach (cb, order);
+      };
+      break;
+      case "pre-order":
+      cb (this.value);
+      if (this.left){
+        this.left.depthFirstForEach(cb, order);
+      };
+      if (this.right) {
+        this.right.depthFirstForEach (cb, order);
+      };
+      break;
+      case "post-order":
+      if (this.left){
+        this.left.depthFirstForEach(cb, order);
+      };
+      if (this.right) {
+        this.right.depthFirstForEach (cb, order);
+      };
+      cb (this.value);
+      break;
+  };
+};
 // let tree = new BinarySearchTree(20)
 // tree.contains(2)
 // tree.insert(2)
