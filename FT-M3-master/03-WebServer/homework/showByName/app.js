f757ec3004ec85d06f9de7034567fb09d03acfab
Facundo Gomez <fgfacundogomez@hotmail.com>
2022-11-06 21:57:40 -0300

    End of HW WebSever

--- FT-M3-master/03-WebServer/homework/showByName/app.js
+++ FT-M3-master/03-WebServer/homework/showByName/app.js
@@ -1,4 +1,24 @@
-var fs  = require("fs")
-var http  = require("http")
+var fs = require("fs");
+var http = require("http");
+//var imagenes = require("./images")
 
 // Escribí acá tu servidor
+
+http
+  .createServer((req, res) => {
+    let info = req.url;
+    if (req.url){
+      fs.readFile(`${__dirname}/images/${info}.jpg`, (err, data) => {
+        if (err) {
+          res.writeHead(404, { "Content-Type": "text/plain" });
+          res.end("Hubo un error, este perro no existe");
+        } else {
+          res.writeHead(200, { "Content-Type": "image/jpg" });
+          res.end(data);
+        }
+      });
+    }
+
+   
+  })
+  .listen(3000, "127.0.0.1");
